9811ad051d80d537c23ca131081df4a26b7416a4
Bhawna-Vishwakarma <bvishwakarma1008@gmail.com>
2024-05-11 23:04:38 +0530

    Program 9

---
+++ BasicThread.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.lang.Thread;
+public class BasicThread extends Thread
+{
+	public static void main(String args[])
+	{
+		BasicThread obj = new BasicThread();
+		System.out.println("Default Name of thread is: "+ obj.getName());
+		obj.setName("Myname");
+		System.out.println("After setting Name of thread is: "+ obj.getName());
+		System.out.println("Default Priority of thread is: "+ obj.getPriority());
+		
+		obj.setPriority(Thread.MAX_PRIORITY);
+		System.out.println("After setting maximum Priority of thread is: "+ obj.getPriority());
+		obj.setPriority(Thread.MIN_PRIORITY);
+		System.out.println("After setting minimum Priority of thread is: "+ obj.getPriority());
+		obj.setPriority(7);
+		System.out.println("After setting custom Priority of thread is: "+ obj.getPriority());
+		
+		System.out.println("Checking Daemon thread is or not : "+ obj.isDaemon());
+
+
+
+		System.out.println("State of thread is: "+ obj.getState());
+	}
+}
